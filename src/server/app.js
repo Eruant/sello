85144e6ec53f4bf4c9ee37c44ec80229496e7747
Matt Gale <matt@littleball.co.uk>
2013-06-05 14:52:18 +0100

    changing server for a simple one

--- src/server/app.js
+++ src/server/app.js
@@ -1,11 +1,46 @@
-/*jslint nomen:true */
-/*globals require, process, __dirname */
+/*globals require, process, console */
 
-var express = require('express'),
-	app = express(),
-	server = require('http').createServer(app),
-	port = process.env.PORT || 8888;
+var http = require('http'),
+	url = require('url'),
+	path = require('path'),
+	fs = require('fs'),
+	port = process.env.PORT || 5000;
 
-server.listen(port);
+http.createServer(function (req, res) {
+	'use strict';
 
-app.use("/", express.static(__dirname + '/public/bin'));
+	var uri = 'bin/' + url.parse(req.url).pathname,
+		filename = path.join(process.cwd(), uri);
+
+	path.exists(filename, function (exists) {
+		if (!exists) {
+			res.writeHead(404, {
+				"Content-Type": "text/plain"
+			});
+			res.write("404 Not Found\n");
+			res.end();
+			return;
+		}
+
+		if (fs.statSync(filename).isDirectory()) {
+			filename += 'index.html';
+		}
+
+		fs.readFile(filename, 'binary', function (err, file) {
+			if (err) {
+				res.writeHead(500, {
+					"Content-Type": "text/plain"
+				});
+				res.write(err + "\n");
+				res.end();
+				return;
+			}
+
+			res.writeHead(200);
+			res.write(file, "binary");
+			res.end();
+		});
+	});
+}).listen(parseInt(port, 10));
+
+console.log("Static file server running at \n => http://localhost:" + port + "\nCTRL + C to shutdown");
