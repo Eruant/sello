7f4924b6a244820807fb517204a50b0e62baff04
Matt Gale <matt@littleball.co.uk>
2013-06-06 12:24:57 +0100

    Added logs for server

--- src/server/sockets.js
+++ src/server/sockets.js
@@ -1,8 +1,12 @@
 var port = process.env.PORT || 80,
 	io = require('socket.io').listen(port);
 
+console.log('<<< started server >>>');
+
 io.set('transports', ['xhr-polling']);
 io.set('polling duration', 10);
+
+console.log('<<< set herou logging >>>');
 
 io.sockets.on('connection', function (socket) {
 	socket.emit('news', { hello: 'world' });
