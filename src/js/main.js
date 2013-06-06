77642953396d04d9466b24cd68c527a42eb44150
Matt Gale <matt@littleball.co.uk>
2013-06-06 12:57:00 +0100

    Adding socket client

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,12 @@
-/*globals window, jQuery, io */
+/*globals jQuery, io */
 
 (function ($, io) {
 	'use strict';
-	
-	window.Chat = {
-		socket: null,
-		
-		init: function (socketURL) {
-			this.socket = io.connect(socketURL);
-			
-			// send message on button click
-			$('send_test').on('click', function () {
-				window.Chat.sent();
-			});
-			
-			// process any incoming messages
-			this.socket.on('new', this.add);
-		},
-		
-		add: function (data) {
-		}
-	};
+
+	var socket = io.connect('http://sello.herokuapp.com');
+	socket.on('news', function (data) {
+		console.log(data);
+		socket.emit('msg', { hello: 'server' });
+	});
+
 }(jQuery, io));
