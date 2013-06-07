23f570881eea3f0b44f376d3a71817ff1cfcb380
Matt Gale <matt@littleball.co.uk>
2013-06-07 17:53:30 +0100

    fix javascript errors

--- src/js/interface.js
+++ src/js/interface.js
@@ -7,14 +7,14 @@
 
 	socket.on('msg', function (data) {
 
-		var html = '<h2>Users</h2>',
+		var html = '<h2>Offices</h2>',
 			offices = data.offices,
 			offices_len = data.offices.length,
 			users_len,
 			i,
 			j;
 		
-		for (i = 0, i < offices_len; i++) {
+		for (i = 0; i < offices_len; i++) {
 			
 			html += '<div>';
 			html += '<h3>' + offices[i].name + '</h3>';
@@ -22,7 +22,7 @@
 			users_len = offices[i].users;
 			
 			html += '<ul>';
-			for(j = 0, j < users_len; j++) {
+			for (j = 0; j < users_len; j++) {
 				html += '<li>' + offices[i].users[j] + '</li>';
 			}
 			html += '</ul>';
